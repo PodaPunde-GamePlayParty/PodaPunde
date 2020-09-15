8a141acf09a6ef23086bd18d71899160bfaf9c0f
harisan02 <531493@edu.rocmn.nl>
2020-09-15 13:40:44 +0200

    -

--- test/app/views/pages/index.php
+++
@@ -1,7 +0,0 @@
-<?php include APPROOT."/views/fragments/header.php"; ?>
-
-<div class="container mt-3">
-    <h1 class="text-center">Dashboard</h1>
-</div>
-
-<?php include APPROOT."/views/fragments/footer.php"; ?>
--- test/app/views/pages/route.php
+++
@@ -1,8 +0,0 @@
-<?php include APPROOT."/views/fragments/header.php"; ?>
-
-<div class="container mt-3">
-    <p class="text-center"><b>Locatie:</b> Harmonielaan 1</p>
-    <p class="text-center"></p>
-</div>
-
-<?php include APPROOT."/views/fragments/footer.php"; ?>
