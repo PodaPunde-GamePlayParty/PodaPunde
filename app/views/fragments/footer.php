2ec46fe65ad1542f3e4af715c90434b1e06df5d6
BobNab <45999993+BobNab@users.noreply.github.com>
2020-09-25 15:00:01 +0200

    Created Footer + Linked Css

--- app/views/fragments/footer.php
+++ app/views/fragments/footer.php
@@ -2,6 +2,68 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
+
+
+<footer class="footer">
+
+  <div class="container">
+    <div class="row">
+
+      <div class="col-6">
+        <h6>About</h6>
+        <p class="text-justify">GamePlayParty <br>
+        Donec ut odio eget odio faucibus commodo et eget tortor. Fusce sollicitudin aliquet congue.
+        Nulla tincidunt pulvinar ex, sed feugiat sapien ornare in. Aliquam gravida porttitor vehicula.
+        In tincidunt pharetra dictum. Ut pretium odio a porta cursus.
+        Sed ornare ante felis, id congue ipsum mollis at.
+        Donec sit amet laoreet nibh.</p>
+      </div>
+
+      <div class="col-6">
+        <h6>Quick Links</h6>
+        <ul class="footer-links">
+          <li><a href="#">#</a></li>
+          <li><a href="#">#</a></li>
+          <li><a href="#">#</a></li>
+          <li><a href="#">#</a></li>
+        </ul>
+      </div>
+
+    </div>
+    
+    <div class="row">
+      <div class="col">
+        <p class="copyright-text">Copyright &copy; 2020 All Rights Reserved by <a href="#">PodaPunde</a></p>
+      </div>
+    </div>
+  
+    <!-- <div class="row"> -->
+
+      <div class="row">
+        <div class="col-1">
+          <a class="facebook" href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+        </div>
+      
+        <div class="col-5">
+          Daily messages will be posted on facebook with updates and fun things
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
+          <a class="twitter" href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+        </div>
+      
+        <div class="col-5">
+          Daily messages will be posted on twitter with updates and fun things
+        </div>
+      </div>
+
+    <!-- </div> -->
+
+  </div>
+</footer>
+
 </body>
 
 </html>
--- app/views/fragments/header.php
+++ app/views/fragments/header.php
@@ -2,13 +2,17 @@
 <html lang="nl">
 
 <head>
+    <?php
+        echo '<title>' . SITENAME . '</title>';
+        echo '<link rel="stylesheet" href="' . URLROOT . '/css/custom.css"';
+    ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
         integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <title><?php echo SITENAME; ?></title>
+        
 </head>
 
 <body class="bg-light">
