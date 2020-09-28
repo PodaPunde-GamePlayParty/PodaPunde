9eaddfeb350770ec18f04b979eb87109bf0e5767
Dyon van Raaij <dyon.a.van.raaij@gmail.com>
2020-09-28 11:04:56 +0200

    header

--- app/views/fragments/header.php
+++ app/views/fragments/header.php
@@ -2,23 +2,16 @@
 <html lang="en">
 
 <head>
-    <?php
-        echo '<title>' . SITENAME . '</title>';
-        echo '<link rel="stylesheet" href="' . URLROOT . '/css/custom.css"';
-    ?>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-<<<<<<< HEAD
 
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/public/css/custom.css">
     <title><?php echo SITENAME; ?></title>
-=======
-        
->>>>>>> origin/bob
 </head>
 
 <body class="m-0">
