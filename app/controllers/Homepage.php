12b944e90ed5103b461fde3208462fc294256809
StrandedHobo <haitambacha0903@hotmail.com>
2020-10-01 20:20:23 +0200

    try

--- app/controllers/Homepage.php
+++ app/controllers/Homepage.php
@@ -3,9 +3,25 @@
 // Create Dashboard class
 class Homepage extends Controller {
 
+    public function __construct() {
+		$this->cinemaModel = $this->model("Homepage");
+	}
+
     // Dashboard page
     public function index() {
-        $this->view("pages/index");
+
+
+        	$this->view("pages/index", $data);
     }
-        
+
+
+    public function homepage() {
+
+        $cinemas = $this->cinemaModel->getCinemas();
+    	$data = [
+    		"cinemas" => $cinemas
+    	];
+
+
+    }
 }
