ac48481968bc1ba29081c4a1aad5e1136359c946
BobNab <45999993+BobNab@users.noreply.github.com>
2020-10-01 20:16:35 +0200

    Nu heb je sws goede db

--- Extra/DataBase/GamePlayPartyStructure.sql
+++ Extra/DataBase/GamePlayPartyStructure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 28 sep 2020 om 11:54
+-- Gegenereerd op: 01 okt 2020 om 20:15
 -- Serverversie: 10.1.38-MariaDB
 -- PHP-versie: 7.3.3
 
@@ -30,12 +30,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cinemas` (
   `Cinema_ID` int(11) NOT NULL,
-  `Hall_ID` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `adress` varchar(80) NOT NULL,
-  `city` varchar(40) NOT NULL,
-  `zipcode` varchar(5) NOT NULL,
-  `province` varchar(20) NOT NULL
+  `name` varchar(20) DEFAULT NULL,
+  `adress` varchar(80) DEFAULT NULL,
+  `city` varchar(40) DEFAULT NULL,
+  `zipcode` varchar(6) DEFAULT NULL,
+  `province` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `customerreservations`
+--
+
+CREATE TABLE `customerreservations` (
+  `Reservation_ID` int(11) DEFAULT NULL,
+  `Customer_ID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -46,14 +56,14 @@ CREATE TABLE `cinemas` (
 
 CREATE TABLE `customers` (
   `Customer_ID` int(11) NOT NULL,
-  `firstname` varchar(40) NOT NULL,
-  `preposition` varchar(10) NOT NULL,
-  `lastName` varchar(40) NOT NULL,
-  `phoneNumber` int(11) NOT NULL,
-  `adress` varchar(80) NOT NULL,
-  `city` varchar(40) NOT NULL,
-  `zipcode` varchar(6) NOT NULL,
-  `province` varchar(20) NOT NULL
+  `firstName` varchar(40) DEFAULT NULL,
+  `preposition` varchar(10) DEFAULT NULL,
+  `lastName` varchar(40) DEFAULT NULL,
+  `phoneNumber` int(11) DEFAULT NULL,
+  `adress` varchar(80) DEFAULT NULL,
+  `city` varchar(40) DEFAULT NULL,
+  `zipcode` varchar(6) DEFAULT NULL,
+  `province` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -63,8 +73,8 @@ CREATE TABLE `customers` (
 --
 
 CREATE TABLE `facilities` (
-  `Hall_ID` int(11) NOT NULL,
-  `facility` varchar(20) NOT NULL
+  `Hall_ID` int(11) DEFAULT NULL,
+  `facility` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -75,11 +85,12 @@ CREATE TABLE `facilities` (
 
 CREATE TABLE `halls` (
   `Hall_ID` int(11) NOT NULL,
-  `hallNumber` int(11) NOT NULL,
-  `quantityChairs` int(11) NOT NULL,
-  `wheelchairAccessible` varchar(5) NOT NULL,
-  `screenSize` varchar(20) NOT NULL,
-  `version` varchar(20) NOT NULL
+  `Cinema_ID` int(11) DEFAULT NULL,
+  `hallNumber` int(11) DEFAULT NULL,
+  `quantityChairs` int(11) DEFAULT NULL,
+  `wheelchairAccessible` varchar(5) DEFAULT NULL,
+  `screenSize` varchar(20) DEFAULT NULL,
+  `version` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -90,10 +101,32 @@ CREATE TABLE `halls` (
 
 CREATE TABLE `prices` (
   `Service_ID` int(11) NOT NULL,
-  `service` varchar(30) NOT NULL,
-  `servicePrice` decimal(4,2) NOT NULL,
-  `regularPrice` decimal(2,2) NOT NULL,
-  `surcharges` decimal(2,2) NOT NULL
+  `service` varchar(30) DEFAULT NULL,
+  `servicePrice` decimal(4,2) DEFAULT NULL,
+  `regularPrice` decimal(2,2) DEFAULT NULL,
+  `surcharges` decimal(2,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `reservationcinemas`
+--
+
+CREATE TABLE `reservationcinemas` (
+  `Reservation_ID` int(11) DEFAULT NULL,
+  `Cinema_ID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `reservationprices`
+--
+
+CREATE TABLE `reservationprices` (
+  `Reservation_ID` int(11) DEFAULT NULL,
+  `Service_ID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -104,12 +137,10 @@ CREATE TABLE `prices` (
 
 CREATE TABLE `reservations` (
   `Reservation_ID` int(11) NOT NULL,
-  `Customer_ID` int(11) NOT NULL,
-  `Service_ID` int(11) NOT NULL,
-  `Cinema_ID` int(11) NOT NULL,
-  `reservationDate` date NOT NULL,
-  `reservationTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `Cinema_ID` int(11) DEFAULT NULL,
+  `ReservationDate` date DEFAULT NULL,
+  `ReservationTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `Date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -123,22 +154,50 @@ ALTER TABLE `cinemas`
   ADD PRIMARY KEY (`Cinema_ID`);
 
 --
+-- Indexen voor tabel `customerreservations`
+--
+ALTER TABLE `customerreservations`
+  ADD KEY `Reservation_ID` (`Reservation_ID`),
+  ADD KEY `Customer_ID` (`Customer_ID`);
+
+--
 -- Indexen voor tabel `customers`
 --
 ALTER TABLE `customers`
   ADD PRIMARY KEY (`Customer_ID`);
 
 --
+-- Indexen voor tabel `facilities`
+--
+ALTER TABLE `facilities`
+  ADD KEY `Hall_ID` (`Hall_ID`);
+
+--
 -- Indexen voor tabel `halls`
 --
 ALTER TABLE `halls`
-  ADD PRIMARY KEY (`Hall_ID`);
+  ADD PRIMARY KEY (`Hall_ID`),
+  ADD KEY `Cinema_ID` (`Cinema_ID`);
 
 --
 -- Indexen voor tabel `prices`
 --
 ALTER TABLE `prices`
   ADD PRIMARY KEY (`Service_ID`);
+
+--
+-- Indexen voor tabel `reservationcinemas`
+--
+ALTER TABLE `reservationcinemas`
+  ADD KEY `Reservation_ID` (`Reservation_ID`),
+  ADD KEY `Cinema_ID` (`Cinema_ID`);
+
+--
+-- Indexen voor tabel `reservationprices`
+--
+ALTER TABLE `reservationprices`
+  ADD KEY `Reservation_ID` (`Reservation_ID`),
+  ADD KEY `Service_ID` (`Service_ID`);
 
 --
 -- Indexen voor tabel `reservations`
@@ -179,6 +238,43 @@ ALTER TABLE `prices`
 --
 ALTER TABLE `reservations`
   MODIFY `Reservation_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `customerreservations`
+--
+ALTER TABLE `customerreservations`
+  ADD CONSTRAINT `customerreservations_ibfk_1` FOREIGN KEY (`Reservation_ID`) REFERENCES `reservations` (`Reservation_ID`),
+  ADD CONSTRAINT `customerreservations_ibfk_2` FOREIGN KEY (`Customer_ID`) REFERENCES `customers` (`Customer_ID`);
+
+--
+-- Beperkingen voor tabel `facilities`
+--
+ALTER TABLE `facilities`
+  ADD CONSTRAINT `facilities_ibfk_1` FOREIGN KEY (`Hall_ID`) REFERENCES `halls` (`Hall_ID`);
+
+--
+-- Beperkingen voor tabel `halls`
+--
+ALTER TABLE `halls`
+  ADD CONSTRAINT `halls_ibfk_1` FOREIGN KEY (`Cinema_ID`) REFERENCES `cinemas` (`Cinema_ID`);
+
+--
+-- Beperkingen voor tabel `reservationcinemas`
+--
+ALTER TABLE `reservationcinemas`
+  ADD CONSTRAINT `reservationcinemas_ibfk_1` FOREIGN KEY (`Reservation_ID`) REFERENCES `reservations` (`Reservation_ID`),
+  ADD CONSTRAINT `reservationcinemas_ibfk_2` FOREIGN KEY (`Cinema_ID`) REFERENCES `cinemas` (`Cinema_ID`);
+
+--
+-- Beperkingen voor tabel `reservationprices`
+--
+ALTER TABLE `reservationprices`
+  ADD CONSTRAINT `reservationprices_ibfk_1` FOREIGN KEY (`Reservation_ID`) REFERENCES `reservations` (`Reservation_ID`),
+  ADD CONSTRAINT `reservationprices_ibfk_2` FOREIGN KEY (`Service_ID`) REFERENCES `prices` (`Service_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
