32e27cacc91a114261f04ac0b94e5eda61e52ade
dyonvanraaij <dyon.a.van.raaij@gmail.com>
2020-10-05 10:42:36 +0200

    Config aangepast

--- app/config/config.php
+++ app/config/config.php
@@ -13,11 +13,15 @@ define("DBPASS","");
 define("DBNAME","gameplayparty");
 
 // Sitename
+<<<<<<< Updated upstream
 define ("SITENAME","gameplayparty");
+=======
+define ("SITENAME","GamePlayParty");
+>>>>>>> Stashed changes
 define ("APPVERSION","0.0.1");
 
 // Folderpaths
 define("APPROOT", dirname(dirname(__FILE__)));
 define("URLROOT", "http://localhost/projecten/gameplayparty/PodaPunde");
 
-// check .htaccess RewriteBase in public folder 
+// check .htaccess RewriteBase in public folder
