33750ac47222fbb259ab47aed1c0b21fb154116e
BobNab <45999993+BobNab@users.noreply.github.com>
2020-10-14 13:27:43 +0200

    Delete All(from bob3) To Merge Fixed into Bob3

--- Extra/SERVER/app/autoloader.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Autoload core libraries
-spl_autoload_register(function ($class) {
-    require_once "libraries/" . $class . ".php";
-});
-
-// Load config
-require_once "config/config.php";
