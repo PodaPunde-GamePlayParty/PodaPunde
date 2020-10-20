c6e9cad1af52b066faa50346a2f48e3325aa93b3
harisan02 <531493@edu.rocmn.nl>
2020-10-21 01:27:02 +0200

    Change Theme of Homepage

--- app/views/index.php
+++ app/views/index.php
@@ -3,86 +3,135 @@
  * Home Page (Content)
  *
  * © 2020 Team PodaPunde
- * 
+ *
  */
 ?>
 
 <?php include APPROOT."/views/fragments/header.php"; ?>
 
-<div class="container mt-lg-3">
-  <h2>Bioscopen</h2>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="<?php echo URLROOT; ?>/public/graph/theatre.jpg" alt="First slide" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo URLROOT; ?>/public/graph/gaming.jpg" alt="Second slide" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo URLROOT; ?>/public/graph/xboxgames.jpg" alt="Second slide" width="1100" height="500">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
+<br>
+<br>
+<div class="container mt-lg-5">
+    <div class="media">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">Komen jullie gamen?</h5>
+      Breng jouw spel naar het volgende niveau op het grote scherm! Met een privé-theater dat speciaal voor jou en je crew is gereserveerd, heb je nog nooit eerder zo gespeeld. Maak er een toernooi van!
+    </div>
+    <img class="ml-3 img-thumbnail" src="<?php echo URLROOT; ?>/public/graph/xbox.jpg" width="300px">
+  </div>
+  </div>
+<br>
+<br>
+<div class="container">
+  <h1 class="text-center"> Populaire games </h1>
+  <br>
+  <div class="container">
+         <div class="card-group">
+             <div class="row">
+                 <div class="card col-md-4">
+                     <img class="card-img-top" src="<?php echo URLROOT; ?>/public/graph/games/minecraft.jpg">
 
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <div class="text-center">
-      <p class=" text-left font-italic text-muted text-justify">
-        <img class=" img-thumbnail float-right" src="<?php echo URLROOT; ?>/public/graph/xboxgames.jpg" width="250px">  
-        Breng jouw spel naar het volgende niveau op het grote scherm!
-        Met een privé-theater dat speciaal voor jou en je crew is gereserveerd,
-        heb je nog nooit eerder zo gespeeld.
-        Maak er een toernooi van!
-        <br>
-        Neem je eigen favoriete Xbox One-spellen mee
-        of kies uit het aanbod van je theater.
-      </p>
+                     <div class="card-body">
+                         <h3 class="card-title font-weight-light">Minecraft</h3>
+                     </div>
+                 </div>
+
+                 <div class="card col-md-4">
+                     <img class="card-img-top" src="<?php echo URLROOT; ?>/public/graph/games/fifa21.jpg">
+
+                     <div class="card-body">
+                         <h3 class="card-title font-weight-light">Fifa 21</h3>
+                     </div>
+                 </div>
+
+                 <div class="card col-md-4">
+                     <img class="card-img-top" src="<?php echo URLROOT; ?>/public/graph/games/forza.jpg">
+
+                     <div class="card-body">
+                         <h3 class="card-title font-weight-light">Forza Horizon 7</h3>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+<br>
+<br>
+     <div class="container">
+            <div class="card-group">
+                <div class="row">
+                    <div class="card col-md-4">
+                        <img class="card-img-top" src="<?php echo URLROOT; ?>/public/graph/games/fortnite.jpg">
+
+                        <div class="card-body">
+                            <h3 class="card-title font-weight-light">Fortnite</h3>
+                        </div>
+                    </div>
+
+                    <div class="card col-md-4">
+                        <img class="card-img-top" src="<?php echo URLROOT; ?>/public/graph/games/blackops.jpg">
+
+                        <div class="card-body">
+                            <h3 class="card-title font-weight-light">Black Ops 4</h3>
+                        </div>
+                    </div>
+
+                    <div class="card col-md-4">
+                        <img class="card-img-top" src="<?php echo URLROOT; ?>/public/graph/games/gta.jpg">
+
+                        <div class="card-body">
+                            <h3 class="card-title font-weight-light">Grand Theft Auto V</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </div>
-    
-    
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <div class="text-center">
-      <p class=" text-left font-italic text-muted text-justify">
-        <img class=" img-thumbnail float-right" src="<?php echo URLROOT; ?>/public/graph/xbox.jpeg" width="250px">  
-        Er is geen minimum voor groepen, maar het wordt aanbevolen dat de groepsgrootte tussen de 8 en 12 personen is.
-        Dit zal de speeltijd voor elke speler maximaliseren.
-        <br>
-        Voel je vrij om je eigen Xbox-spel mee te nemen om te spelen
-        (persoonlijke spelconsoles of spellen voor andere spelconsoles zijn niet toegestaan).
-        <br>
-        Er is geen leeftijdsgrens voor videospelletjes op Xbox,
-        maar de ouders moeten wel zelf kunnen beslissen over de spelbeoordeling van oudere gamers
-        (d.w.z., titels met een 'M'-rating).
-        <br>
-        Feesten kunnen maximaal 6 weken voor de datum van uw voorkeur worden aangevraagd,
-        en zijn niet gegarandeerd tot de datum is bevestigd en geboekt door het theater.
-        <br>
-        Voor elke partij kan een aanbetaling van $50 worden gevraagd en kan op elk moment voor de partij aan de kassa worden betaald.
-        De boeking kan pas worden gereserveerd na ontvangst van de aanbetaling.
-        <br>
-      </p>
-    </div>
+<br>
+<br>
+<div class="container mt-lg-5">
+    <div class="media">
+    <div class="media-body">
+      <h5 class="mt-0 mb-1">Dingen die u moet weten, voordat u een bestelling plaatst!</h5>
+          <p class="font-italic">  Er is geen minimum voor groepen, maar het wordt aanbevolen dat de groepsgrootte tussen de 8 en 12 personen is. Dit zal de speeltijd voor elke speler maximaliseren.
+      	    Voel je vrij om je eigen Xbox-spel mee te nemen om te spelen (persoonlijke spelconsoles of spellen voor andere spelconsoles zijn niet toegestaan).
+      	    Er is geen leeftijdsgrens voor videospelletjes op Xbox, maar de ouders moeten wel zelf kunnen beslissen over de spelbeoordeling van oudere gamers (d.w.z., titels met een 'M'-rating).
+      	    Feesten kunnen maximaal 6 weken voor de datum van uw voorkeur worden aangevraagd en zijn niet gegarandeerd tot de datum is bevestigd en geboekt door het theater.
+      	    Voor elke partij kan een aanbetaling van $50 worden gevraagd en kan op elk moment voor de partij aan de kassa worden betaald. De boeking kan pas worden gereserveerd na ontvangst van de aanbetaling.
+      	    Annuleringen met een opzegtermijn van minder dan 24 uur kunnen leiden tot een niet-terugvorderbare aanbetaling.
+      	    Buiten eten en drinken is niet toegestaan in de theatercomplexen, maar als u een verjaardag viert, kunt u uw eigen verjaardagstaart meenemen! Wij zorgen voor de borden, servetten en bestek.
+      	    Feesten kunnen op elk moment buiten de openingsuren geboekt worden. Een voorbeeldboeking is zaterdag 10.00 - 12.00 uur of eender welke nacht na 22.30 uur, in afwachting van beschikbaarheid in bepaalde theaters en kan rechtstreeks bij het theater worden bevestigd.
+      	    Cadeaubonnen, alle belangrijke creditcards en debetkaarten worden geaccepteerd als betaalmiddel.
+      	    Het is mogelijk dat er op sommige locaties doordeweeks geen partyboekingen beschikbaar zijn. </p>   </div>
   </div>
-</div>
-  
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <div class="text-center">
-      <p class=" text-left font-italic text-muted text-justify">
-        <img class=" img-thumbnail float-right" src="<?php echo URLROOT; ?>/public/graph/cancel.jpg" width="250px">  
-        Annuleringen met een opzegtermijn van minder dan 24 uur kunnen leiden tot een niet-terugvorderbare aanbetaling.
-        <br>
-        Buiten eten en drinken is niet toegestaan in de theatercomplexen,
-        maar als u een verjaardag viert, kunt u uw eigen verjaardagstaart meenemen!
-        Wij zorgen voor de borden, servetten en bestek.
-        <br>
-        Feesten kunnen op elk moment buiten de openingsuren geboekt worden.
-        Een voorbeeldboeking is zaterdag 10.00 - 12.00 uur of eender welke nacht na 22.30 uur,
-        in afwachting van beschikbaarheid in bepaalde theaters en kan rechtstreeks bij het theater worden bevestigd.
-        <br>
-        Cadeaubonnen, alle belangrijke creditcards en debetkaarten worden geaccepteerd als betaalmiddel.
-        <br>
-        Het is mogelijk dat er op sommige locaties doordeweeks geen partyboekingen beschikbaar zijn.
-        <br>
-        * Xbox Live kan afhankelijk van de huidige internetconnectiviteit en internetsnelheden variëren tussen de deelnemende locaties.
-        <br>
-        * De vereisten en beschikbare functies voor Xbox Live-gameplay voor meerdere spelers verschillen per systeem en speltitel.
-        <br>
-      </p>
-    </div>
   </div>
-</div>
+
 
 <?php include APPROOT."/views/fragments/footer.php"; ?>
